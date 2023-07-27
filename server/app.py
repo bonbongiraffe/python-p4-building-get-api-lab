c0c296d695252dec62e84ee2c4fd1f42150fe32e
bonbongiraffe <francescowai@yahoo.com>
2023-07-27 10:44:58 -0400

    First attempt

--- server/app.py
+++ server/app.py
@@ -9,10 +9,24 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+app.debug = True
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+def baked_goods_list(baked_goods):
+    output_list = []
+    for baked_good in baked_goods:
+        baked_good_dict = {
+            "id": baked_good.id,
+            "name": baked_good.name,
+            "price": baked_good.price,
+            "created_at": baked_good.created_at,
+            "updated_at": baked_good.updated_at
+        }
+        output_list.append(baked_good_dict)
+    return output_list
 
 @app.route('/')
 def index():
@@ -20,19 +34,51 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = {
+            "id": bakery.id,
+            "name": bakery.name,
+            "created_at": bakery.created_at,
+            "updated_at": bakery.updated_at
+        }
+        bakeries.append(bakery_dict)
+    # bakeries = [b.to_dict() for b in Bakery.query.all()]
+    response = make_response(jsonify(bakeries), 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    baked_goods = BakedGood.query.filter(BakedGood.bakery_id == id).all()
+    bakery_dict = {
+        "id": bakery.id,
+        "name": bakery.name,
+        "created_at": bakery.created_at,
+        "updated_at": bakery.updated_at,
+        "baked_goods": baked_goods_list(baked_goods)
+    }
+    response = make_response(jsonify(bakery_dict), 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price).all()
+    response = make_response(jsonify(baked_goods_list(baked_goods)), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    baked_good_dict = {
+            "id": baked_good.id,
+            "name": baked_good.name,
+            "price": baked_good.price,
+            "created_at": baked_good.created_at,
+            "updated_at": baked_good.updated_at
+        }
+    response = make_response(jsonify(baked_good_dict), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
